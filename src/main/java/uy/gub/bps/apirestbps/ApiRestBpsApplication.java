4a94a5249d42df81a46a9a82a6a8e2693773e8aa
LUIS SALVATIERRA <luisangel.salvat@gmail.com>
2023-08-28 09:15:38 -0500

    Initial commit

---
+++ src/main/java/uy/gub/bps/apirestbps/ApiRestBpsApplication.java
@@ -0,0 +1,13 @@
+package uy.gub.bps.apirestbps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestBpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestBpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uy/gub/bps/apirestbps/ApiRestBpsApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.gub.bps.apirestbps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestBpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
