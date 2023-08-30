f1594c2027d84f7b518c5a2ab2461f37ca6eccdd
LUIS SALVATIERRA <luisangel.salvat@gmail.com>
2023-08-30 11:08:23 -0500

    Operaciones POST y PUT.

--- src/main/java/uy/gub/bps/apirestbps/controller/EstadoController.java
+++ src/main/java/uy/gub/bps/apirestbps/controller/EstadoController.java
@@ -3,10 +3,7 @@ package uy.gub.bps.apirestbps.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import uy.gub.bps.apirestbps.exception.ResourceNotFoundException;
 import uy.gub.bps.apirestbps.model.Estado;
 import uy.gub.bps.apirestbps.service.EstadoService;
@@ -35,6 +32,21 @@ public class EstadoController {
                 .orElseThrow(()-> new ResourceNotFoundException("Objeto con el ID="
                         + id +", no encontrado."));
         return new ResponseEntity<>(estado, HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Estado> crearEstado(@RequestBody Estado estado){
+        Estado newEstado = estadoService.guardar(new Estado(estado.getDescestado()));
+        return new ResponseEntity<>(newEstado, HttpStatus.CREATED);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Estado> actualizarEstado(@PathVariable("id") long id,
+                                                   @RequestBody Estado estado){
+        Estado currentEstado = estadoService.obtenerPorId(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Objeto con el ID="
+                        + id +", no encontrado."));
+        currentEstado.setDescestado(estado.getDescestado());
+        return new ResponseEntity<>(estadoService.guardar(currentEstado), HttpStatus.OK);
     }
 
 
