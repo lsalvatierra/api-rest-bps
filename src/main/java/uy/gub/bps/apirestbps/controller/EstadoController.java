42153c96eb121a9ef4c0b8392b63784148238bea
LUIS SALVATIERRA <luisangel.salvat@gmail.com>
2023-08-30 10:50:58 -0500

    Operaciones GET.

--- src/main/java/uy/gub/bps/apirestbps/controller/EstadoController.java
+++ src/main/java/uy/gub/bps/apirestbps/controller/EstadoController.java
@@ -1,16 +1,42 @@
 package uy.gub.bps.apirestbps.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uy.gub.bps.apirestbps.exception.ResourceNotFoundException;
+import uy.gub.bps.apirestbps.model.Estado;
+import uy.gub.bps.apirestbps.service.EstadoService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping(path = "apibps/v1")
+@RequestMapping(path = "api/v1/estado")
 public class EstadoController {
-
-    @GetMapping("/saludo")
-    public String saludar(){
-        return "Hola Rest";
+    @Autowired
+    private EstadoService estadoService;
+    @GetMapping("")
+    public ResponseEntity<List<Estado>> obtenerTodo(){
+        List<Estado> estados = new ArrayList<>();
+        estadoService.obtenerTodo().forEach(estados::add);
+        if(estados.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(estados, HttpStatus.OK);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Estado> obtenerEstadoPorId(@PathVariable("id") long id){
+        Estado estado = estadoService.obtenerPorId(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Objeto con el ID="
+                        + id +", no encontrado."));
+        return new ResponseEntity<>(estado, HttpStatus.OK);
+    }
+
+
 
 }
