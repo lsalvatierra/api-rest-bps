9764124786dee6e234120937a1190e775beff1a0
LUIS SALVATIERRA <luisangel.salvat@gmail.com>
2023-09-06 11:07:12 -0500

    Cliente: nuevos registros SQL y Controlador pageable.

--- src/main/java/uy/gub/bps/apirestbps/controller/ClienteController.java
+++ src/main/java/uy/gub/bps/apirestbps/controller/ClienteController.java
@@ -1,12 +1,11 @@
 package uy.gub.bps.apirestbps.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import uy.gub.bps.apirestbps.exception.ResourceNotFoundException;
 import uy.gub.bps.apirestbps.model.Cliente;
 import uy.gub.bps.apirestbps.service.ClienteService;
@@ -27,6 +26,19 @@ public class ClienteController {
                 .orElseThrow(() -> new ResourceNotFoundException("Cliente con el documento ="+
                         documento+" no encoentrado"));
         return new ResponseEntity<>(cliente, HttpStatus.OK);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<?> buscarClientexNombre(
+            @RequestParam String nombre,
+            Pageable pageable
+    ){
+        Page<Cliente> clientes = clienteService.buscarClienteXNombre(nombre,
+                pageable);
+        if(clientes.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(clientes, HttpStatus.OK);
     }
 
 
